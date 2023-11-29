b927daf9ce4ce892fe0609b9da16c360d3afbe31
marcos <marcosmendezser1@gnail.com>
2023-11-30 00:01:56 +0100

    Remove Validate Functions

--- src/InventaryShop/Product/Domain/ValueObjects/ProductPrice.php
+++ src/InventaryShop/Product/Domain/ValueObjects/ProductPrice.php
@@ -1,33 +1,14 @@
 <?php
 
-namespace src\InventaryShop\Product\Domain\ValueObjects;
-
-use InvalidArgumentException;
-use Ramsey\Uuid\Type\Decimal;
+namespace Src\InventaryShop\Product\Domain\ValueObjects;
 
 final class ProductPrice
 {
     private $value;
 
-    public function __construct(Decimal $price)
+    public function __construct(string $price)
     {
-        $this->validate($price);
         $this->value = $price;
-    }
-
-    private function validate(Decimal $price)
-    {
-        $options = array(
-            'options' => array(
-                'min_range' => 0,
-            ),
-        );
-
-        if (!filter_var($price, FILTER_VALIDATE_INT, $options)) {
-            throw new InvalidArgumentException(
-                sprintf('<%s> does not allow the value <%s>.', static::class, $price)
-            );
-        }
     }
 
     public function value()
--- src/InventaryShop/Product/Domain/ValueObjects/ProductStock.php
+++ src/InventaryShop/Product/Domain/ValueObjects/ProductStock.php
@@ -1,32 +1,14 @@
 <?php
 
-namespace src\InventaryShop\Product\Domain\ValueObjects;
-
-use InvalidArgumentException;
+namespace Src\InventaryShop\Product\Domain\ValueObjects;
 
 final class ProductStock
 {
     private $value;
 
-    public function __construct(int $stock)
+    public function __construct(string $stock)
     {
-        $this->validate($stock);
         $this->value = $stock;
-    }
-
-    private function validate(int $stock)
-    {
-        $options = array(
-            'options' => array(
-                'min_range' => 0,
-            ),
-        );
-
-        if (!filter_var($stock, FILTER_VALIDATE_INT, $options)) {
-            throw new InvalidArgumentException(
-                sprintf('<%s> does not allow the value <%s>.', static::class, $stock)
-            );
-        }
     }
 
     public function value()
