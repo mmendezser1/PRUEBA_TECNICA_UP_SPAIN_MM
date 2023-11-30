a20990c49399768f3b3132babed15e79d7f9608e
marcos <marcosmendezser1@gnail.com>
2023-11-30 13:23:15 +0100

    Product Model: timestamps = false

--- app/Product.php
+++ app/Product.php
@@ -8,12 +8,8 @@ class Product extends Model
 {
 
     protected $table = 'products';
+    public $timestamps = false;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'description', 'price', 'stock',
     ];
